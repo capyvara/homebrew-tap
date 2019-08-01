890a84441285fd3bd3575381b24f816a99708df1
Marcelo Oliveira <capyvara@gmail.com>
2019-08-01 18:00:35 -0300

    Bump to 1.0.2

--- unityyamlmerge.rb
+++ unityyamlmerge.rb
@@ -2,8 +2,8 @@ class Unityyamlmerge < Formula
   desc "Wrapper for brew install of UnityYAMLMerge"
   homepage "https://docs.unity3d.com/Manual/SmartMerge.html"
   head "https://github.com/capyvara/unityyamlmerge.git"
-  version "1.0.1"
-  sha256 "bb976b94748875355efbee84e76c96f91d50305e0601734e1a0ac638f0e22f3e"
+  version "1.0.2"
+  sha256 "0100b3a17416a424e9e03d454b7b036639392b438571142902182a17340f85ea"
   url "https://github.com/capyvara/unityyamlmerge/releases/download/v#{version}/unityyamlmerge.tar.gz"
 
   def install
